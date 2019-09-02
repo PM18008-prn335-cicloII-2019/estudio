d9925f6c7410254cf767a3861db013e2642b7eda
user <user@com1pc10>
2019-09-02 15:29:30 -0600

    primer commit

---
+++ src/main/java/ues/occ/edu/sv/ingenieria/prn335/guia03/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package ues.occ.edu.sv.ingenieria.prn335.guia03;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
